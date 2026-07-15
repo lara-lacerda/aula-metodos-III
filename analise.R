f2dc110e50272f04538bfd848563a1338122590f
lara-lacerda <lara1202n@gmail.com>
2026-07-14 22:52:41 -0300

    trabalho final

--- analise.R
+++ analise.R
@@ -4,3 +4,8 @@ teste + 2
 a <- 5
 a + 50
 1 + 3 + 4
+
+pastas <- c("data/raw", "data/processed", "scripts", "outputs", "paper")
+for (p in pastas) dir.create(p, recursive = TRUE)
+file.create("scripts/01_clean.R")
+file.create("scripts/02_analysis.R")
